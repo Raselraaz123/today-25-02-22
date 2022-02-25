7e8f5ab044b7178c90034effa3d4a43cd5e568fa
Raselraaz123 <82328669+Raselraaz123@users.noreply.github.com>
2022-02-25 20:27:04 +0600

    all not ok

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Rasel Raaz')
+document.getElementById('getAll-info').addEventListener('click', function () {
+    // console.log('rasel raaz')
+    const getinputValueName = document.getElementById('exampleFormControlInput1')
+    const inputValueName = getinputValueName.value
+    console.log(inputValueName)
+
+    const getinputValueEmail = document.getElementById('exampleFormControlInput2')
+    const inputValueEmail = getinputValueEmail.value
+    console.log(inputValueEmail)
+
+    const getinputValuepassword = document.getElementById('exampleFormControlInput3')
+    const inputValuepassword = getinputValuepassword.value
+    console.log(inputValueEmail)
+})
